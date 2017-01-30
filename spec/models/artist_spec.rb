3ad97a7598cd0397fb00e40e32f343f897fd3220
Haico27 <haico.wensink@gmail.com>
2017-01-30 09:42:48 +0100

    Added tests

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  it "has many songs" { is_expected.to have_many(:songs) }
-  it "is invalid without a name" { is expected.to validate_presence_of :name }
+  describe "association with song" do
+    let(:artist) { create :artist }
+    let!(:song) { create :song, artist: artist }
+
+  it "has many songs" do
+    song1 = artist.songs.new( name: "No Control" )
+    song2 = artist.songs.new( name: "Billy")
+
+    expect(artist.songs).to include(song1)
+    expect(artist.songs).to include(song2)
+end
+
+
+
+  end
+
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,6 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  it { is_expected.to belong_to(:artist) }
-  it { is expected.to validates_presence_of :name }
+  describe "validations" do
+    it "is invalid without a name" do
+      song = Song.new(name: "")
+      song.invalid?
+      expect(song.errors).to have_key(:name)
+    end
+  end
+
+
+
+  describe "association with artist" do
+    let(:artist) { create :artist }
+
+  it "belongs to an artist" do
+    song = artist.songs.new(name: "Heaven Knows")
+
+    expect(song.artist).to eq(artist)
+  end
+end
+
 end
